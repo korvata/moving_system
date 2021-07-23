99dddce0a45b0ea66fbd17aa1d07eb2b95c5ce85
korvata <korvata@naver.com>
2021-07-24 01:54:56 +0900

    고객 서비스 로그 제거

--- src/main/java/marketdesigners/subject/service/UserService.java
+++ src/main/java/marketdesigners/subject/service/UserService.java
@@ -1,14 +1,12 @@
 package marketdesigners.subject.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import marketdesigners.subject.domain.User;
 import marketdesigners.subject.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
