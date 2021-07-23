e2b1403562ef96f3f98e2846238f95a7a6ac66a7
korvata <korvata@naver.com>
2021-07-24 01:54:02 +0900

    고객 엔티티 등록일자 날짜 형식 추가

--- src/main/java/marketdesigners/subject/domain/User.java
+++ src/main/java/marketdesigners/subject/domain/User.java
@@ -1,5 +1,6 @@
 package marketdesigners.subject.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,6 +21,7 @@ public class User {
 
     private String tel;                 //연락처
 
+    @JsonFormat(pattern = "yyyy-MM-dd")
     @Column(name="reg_date")
     private LocalDateTime regDate;      //등록일자
 
