ac5ad307cd851bf806a2cb575aa1e055fcb0887b
korvata <korvata@naver.com>
2021-07-24 01:45:32 +0900

    고객 엔티티 수정 및 생성 메서드 추가

--- src/main/java/marketdesigners/subject/domain/User.java
+++ src/main/java/marketdesigners/subject/domain/User.java
@@ -16,6 +16,8 @@ public class User {
     @Column(name = "user_id")
     private Long id;                    //고객 ID
 
+    private String name;                //고객 이름
+
     private String tel;                 //연락처
 
     @Column(name="reg_date")
@@ -28,4 +30,27 @@ public class User {
     @OneToMany
     @JoinColumn(name="reg_id")
     private Registration registration;  //신청접수 정보
+
+    @Column(name = "use_agree_status")
+    private Status useAgreeStatus;      //이용약관동의여부
+
+    @Column(name = "third_agree_status")
+    private Status thirdAgreeStatus;    //제3자 동의여부
+
+    @Column(name = "market_agree_status")
+    private Status marketAgreeStatus;   //마케팅 정보수신 동의여부
+
+    /**
+     * 생성 메서드
+     */
+    public User(String name, String tel, LocalDateTime regDate, Feedback feedback, Registration registration, Status useAgreeStatus, Status thirdAgreeStatus, Status marketAgreeStatus) {
+        this.name = name;
+        this.tel = tel;
+        this.regDate = regDate;
+        this.feedback = feedback;
+        this.registration = registration;
+        this.useAgreeStatus = useAgreeStatus;
+        this.thirdAgreeStatus = thirdAgreeStatus;
+        this.marketAgreeStatus = marketAgreeStatus;
+    }
 }
