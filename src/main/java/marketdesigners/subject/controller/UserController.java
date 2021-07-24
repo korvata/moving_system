5b8ace1d8a71406f23c6b479db983a7bd739f296
korvata <korvata@naver.com>
2021-07-24 23:36:48 +0900

    고객 정보 조회 API

---
+++ src/main/java/marketdesigners/subject/controller/UserController.java
@@ -0,0 +1,23 @@
+package marketdesigners.subject.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import marketdesigners.subject.domain.User;
+import marketdesigners.subject.mapper.UserMapper;
+import marketdesigners.subject.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/user")
+    public List<UserMapper> findAllBy(){
+
+        return userService.findAllUserMapper();
+    }
+}
