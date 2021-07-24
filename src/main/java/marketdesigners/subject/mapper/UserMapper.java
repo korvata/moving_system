1b9c45259160de1e00664d33971d45c5e6b3e34a
korvata <korvata@naver.com>
2021-07-24 23:35:26 +0900

    출력하고자 하는 고객 정보(이름, 연락처)

---
+++ src/main/java/marketdesigners/subject/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package marketdesigners.subject.mapper;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserMapper {
+    private String name;
+    private String tel;
+}
