3ad1579162e90301dca92b0756645dd3a718d95f
korvata <korvata@naver.com>
2021-07-24 23:34:00 +0900

    연락처 마스킹 처리 로직 추가

--- src/main/java/marketdesigners/subject/service/UserService.java
+++ src/main/java/marketdesigners/subject/service/UserService.java
@@ -2,9 +2,11 @@ package marketdesigners.subject.service;
 
 import lombok.RequiredArgsConstructor;
 import marketdesigners.subject.domain.User;
+import marketdesigners.subject.mapper.UserMapper;
 import marketdesigners.subject.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,7 +15,28 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public List<User> findAll(){                       //고객 전체 조회
+    public List<UserMapper> findAllUserMapper(){                       //고객 전체 조회
+
+        List<UserMapper> userMapperList = new ArrayList<>();
+
+        List<User> users = findAll();
+
+        for(User user : users){
+            UserMapper userMapper = new UserMapper();
+            String name = user.getName();
+            String tel = user.getTel();
+            String newTel = tel.substring(0, tel.length()-3) + "**"+ tel.substring(tel.length()-2 + 1);
+
+            userMapper.setName(name);
+            userMapper.setTel(newTel);
+
+            userMapperList.add(userMapper);
+        }
+
+        return userMapperList;
+    }
+
+    public List<User> findAll(){
 
         return userRepository.findAll();
     }
