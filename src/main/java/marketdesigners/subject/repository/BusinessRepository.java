93853894446230f41eb762e8f8c73248a8992dd6
korvata <korvata@naver.com>
2021-07-25 11:57:55 +0900

    Transactional 수정

--- src/main/java/marketdesigners/subject/repository/BusinessRepository.java
+++ src/main/java/marketdesigners/subject/repository/BusinessRepository.java
@@ -9,7 +9,7 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
-@Transactional
+@Transactional(readOnly = false)
 public class BusinessRepository {
 
     @PersistenceContext
